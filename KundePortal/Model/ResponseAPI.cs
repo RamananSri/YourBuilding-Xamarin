038416aa85e75ac6d9f519201f10c4f367f8da56
RamananSri <1058515@ucn.dk>
2017-11-22 09:17:12 +0100

    api

---
+++ KundePortal/Model/ResponseAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace KundePortal.Model
+{
+    public class ResponseAPI
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+
+        public ResponseAPI()
+        {
+        }
+    }
+}
