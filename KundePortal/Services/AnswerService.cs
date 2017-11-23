7545eacc5c310b49d9a4f60991081efa6d00c62f
Daniel070793 <daniel070793@gmail.com>
2017-11-23 09:04:23 +0100

    sqllite?

--- KundePortal/Services/AnswerService.cs
+++ KundePortal/Services/AnswerService.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using KundePortal.Model;
 namespace KundePortal.Services
 {
     public class AnswerService
     {
+        APIService API;
         public AnswerService()
         {
+            API = new APIService();
         }
+
+        async public Task<ResponseAPI> postAnswer(AnswerModel answer)
+        {
+            answer = new AnswerModel();
+            ResponseAPI res = API.post("api/answers/");
+            if (res.token != null && res.user != null)
+            {
+                APIService.token = res.token;
+                APIService.currentUser = res.user;
+            }
+            return res;
+        }
+    }
+
     }
 }
