d8bb1a2d83ec1605331b0b910067682c321f3567
Daniel070793 <daniel070793@gmail.com>
2017-12-08 09:28:42 +0100

    fixed frederiks shit

--- KundePortal/ViewModel/ProMenuViewModel.cs
+++ KundePortal/ViewModel/ProMenuViewModel.cs
@@ -1,72 +1,58 @@
 ﻿using System;
 using System.Windows.Input;
-using KundePortal.Utility;
 using KundePortal.View;
 using Xamarin.Forms;
+using KundePortal.Utility;
 
 namespace KundePortal.ViewModel
 {
     public class ProMenuViewModel
     {
-        public ICommand QuestionsViewCommand { get; set; }
-        //public ICommand GPSViewCommand { get; set; }
-        public ICommand CategoriesViewCommand { get; set; }
-        public ICommand viewUserCommand { get; set; }
-        public ICommand LogOutCommand { get; set; }
+        string _username;
 
-        INavigation nav;
+        public ICommand SubsViewCommand { get; set; }
+        public ICommand CategoryViewCommand { get; set; }
+        public ICommand AccountViewCommand { get; set; }
 
         public ProMenuViewModel()
         {
-            QuestionsViewCommand = new Command(NavigateToQuestions);
-            CategoriesViewCommand = new Command(NavigateToCategories);
-            viewUserCommand = new Command(ViewUser);
-            LogOutCommand = new Command(LogOut);
-            nav = Application.Current.MainPage.Navigation;
+            _username = APIService.currentUser.name;
+            SubsViewCommand = new Command(NavigateToSubs);
+            CategoryViewCommand = new Command(NavigateToCategories);
+            AccountViewCommand = new Command(NavigateToAccount);
         }
 
-        async void ViewUser()
+        async void NavigateToSubs()
         {
-            await nav.PushAsync(new ViewUserView());
-        }
-
-        async void NavigateToQuestions()
-        {
-            await nav.PushAsync(new CategoryView());
+            INavigation nav = Application.Current.MainPage.Navigation;
+            await nav.PushAsync(new ProMyCategoriesView());
         }
 
         async void NavigateToCategories()
         {
-            await nav.PushAsync(new ProMyCategoriesView());
+            INavigation nav = Application.Current.MainPage.Navigation;
+            await nav.PushAsync(new CategoryView());
         }
 
-        async void LogOut()
+        async void NavigateToAccount()
         {
-            APIService.currentUser = null;
-            APIService.token = null;
-            await nav.PopToRootAsync();
+            INavigation nav = Application.Current.MainPage.Navigation;
+            await nav.PushAsync(new ViewUserView());
+        }
+        #region Properties
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+            }
         }
 
-        //async void Navigate(string caller)
-        //{
-        //    INavigation nav = Application.Current.MainPage.Navigation;
-
-        //    switch (caller)
-        //    {
-        //        case "Beskeder":
-        //            await nav.PushAsync(new ProNotificationsView());
-        //            break;
-        //        case "Alle spørgsmål":
-        //            await nav.PushAsync(new ProAllQuestionsView());
-        //            break;
-        //        case "I nærheden":
-        //            await nav.PushAsync(new ProGPSView());
-        //            break;
-        //        case "Kategorier":
-        //            await nav.PushAsync(new ProMyCategoriesView());
-        //            break;
-
-        //    }
-        //}
+        #endregion
     }
 }
